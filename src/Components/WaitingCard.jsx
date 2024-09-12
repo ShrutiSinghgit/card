82bc7f44050a6d457bca8100ba6e37b66cc4f401
ShrutiSinghgit <140903675+ShrutiSinghgit@users.noreply.github.com>
2024-09-12 18:37:54 +0530

    Dropdown

---
+++ src/Components/WaitingCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const WaitingCard = () => {
+  return (
+     <div className="booking-card">
+    <div className="card-header">
+      <span className="booking-id">OD 18N 6616</span>
+      <span className="check-icon">✔️</span> 
+    </div>
+
+
+    <div className="card-body">
+      <div className="card-detail">
+        <span className="car-color">
+        Black  <span className="color-dot black"></span>
+        </span>
+        <span id="hash-design">|</span>
+        <span className="seat-detail">
+          <strong>06</strong>, A2 <span className="floor-info">ground floor</span>
+        </span>
+      </div>
+
+
+      <div className="booking-info">
+        Booked: <strong>Via Online</strong>
+      </div>
+    </div>
+    <div className="card-footer">
+      <span>Booked 25 min ago</span>
+    </div>
+  </div>
+  )
+}
